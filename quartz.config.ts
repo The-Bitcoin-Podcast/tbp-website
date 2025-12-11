1455017c57d2b5e6e829aa585dbe7a8f13f00c93
Corey Petty <corey@status.im>
2025-12-11 10:27:16 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "the-bitcoin-podcast.github.io/tbp-website",
+    baseUrl: "thebitcoinpodcast.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
