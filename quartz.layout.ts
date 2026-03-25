37281a25048eb190fd71f3138d2d041911d443aa
Corey Petty <corey@status.im>
2026-03-25 15:28:33 +0000

    fix: explorer defaults to collapsed

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -54,7 +54,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ folderDefaultState: "collapsed", useSavedState: false }),
   ],
   right: [
     Component.Graph({
@@ -82,7 +82,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ folderDefaultState: "collapsed", useSavedState: false }),
   ],
   right: [],
 }
